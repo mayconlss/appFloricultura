fa784afbf55b7ac97021a985c4baf68f3e629858
Maycon <maycon.souza@al.infnet.edu.br>
2020-12-08 17:58:44 -0300

    AT FundJava

---
+++ src/main/java/br/edu/infnet/appFloricultura/AppFloriculturaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appFloricultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFloriculturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFloriculturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appFloricultura/AppFloriculturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appFloricultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFloriculturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
